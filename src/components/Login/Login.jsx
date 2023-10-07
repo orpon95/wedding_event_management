e12359777d072486fa0d58f8b829e8baf501777d
orpon95 <mdorpon95@gmail.com>
2023-10-08 01:44:10 +0600

    added sweet alert when error happned and when succesfully registered or logged in

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,12 +2,14 @@
 import React, { useContext, useState } from 'react';
 import { authContext } from '../../AuthProvider/AuthProvider';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { Component } from 'react';
+import Swal from 'sweetalert2';
 
 const Login = () => {
-    const {user} = useContext(authContext)
+    const { user } = useContext(authContext)
     const nevigate = useNavigate()
     const location = useLocation()
-    // console.log(location);
+    console.log(location);
     const [errors, setError] = useState(null)
     const { signin } = useContext(authContext)
 
@@ -23,16 +25,33 @@ const Login = () => {
 
         // setError("")
         signin(email, password)
-            .then(result => console.log(result.user))
-            .catch(err => setError(err.message))
+            .then(result => {
 
-            if(user){
+                console.log(result.user)
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'sucessfully Logged in',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+
                 nevigate(location?.state ? location.state : "/")
-            }
-            else{
-                nevigate("/Login")
-            }
-        
+
+            })
+            .catch(err => {
+                setError(err.message)
+                Swal.fire({
+                    title: `${errors}`,
+                    text: '',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                })
+
+            })
+
+
+
 
 
     }
@@ -62,50 +81,50 @@ const Login = () => {
 
 
 
-return (
-    <div>
-        {/* <h1>{ 
+    return (
+        <div>
+            {/* <h1>{ 
             error ? <h1>invalid pass or email</h1> : <h2>done</h2>
             
             
             
             }</h1> */}
-        <h1>{errors}</h1>
-        <h1 className="text-5xl font-bold text-center">Log In here!!!</h1>
-        <div className="hero min-h-screen bg-base-200">
+            
+            <h1 className="text-5xl font-bold text-center">Log In here!!!</h1>
+            <div className="hero min-h-screen bg-base-200">
 
-            <div className="hero-content ">
+                <div className="hero-content ">
 
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleLogin} className="card-body">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
 
-                        {/* email field */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input name='email' type="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        {/* password field */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input name='password' type="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        {/* button */}
-                        <div className="form-control mt-6">
-                            <button type='submit' className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
+                            {/* email field */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            {/* password field */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='password' type="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            {/* button */}
+                            <div className="form-control mt-6">
+                                <button type='submit' className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext, useState } from 'react';
 import { authContext } from '../../AuthProvider/AuthProvider';
+import  { Component } from 'react';
+import Swal from 'sweetalert2';
+
 
 const Register = () => {
     const [errors, setError] = useState("")
@@ -16,6 +19,13 @@ const Register = () => {
         // password validation
         if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(password)) {
             setError("password required Minimum six characters, at least one uppercase letter, one lowercase letter, one number and one special character:")
+            Swal.fire({
+                title: `${errors}`,
+                text: '',
+                icon: 'error',
+                confirmButtonText: 'OK'
+              })
+ 
         }
         else {
 
@@ -24,6 +34,13 @@ const Register = () => {
             createUser(email, password)
                 .then(result => console.log(result.user))
                 .catch(error => console.error(error))
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'sucessfully registered',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
         }
 
 
@@ -35,7 +52,7 @@ const Register = () => {
     return (
         <div>
             <h1 className="text-5xl font-bold text-center">Register Here!!!</h1>
-            <h1>{errors}</h1>
+           
             <div className="hero min-h-screen bg-base-200">
 
                 <div className="hero-content ">
